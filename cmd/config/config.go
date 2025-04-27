5f9143c003bc4c91f6796b9f610e1ce87d30afda
evgeny.golovkov <evgeny.golovkov@axenix.pro>
2025-04-27 13:49:31 +0300

    add env

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -3,30 +3,46 @@ package config
 import (
 	"flag"
 	"fmt"
+	"github.com/caarlos0/env/v6"
 	"strings"
 )
 
 type Config struct {
-	ServerAddress string
-	BaseURL       string
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
 }
 
 func LoadConfig() (*Config, error) {
-	address := flag.String("a", "localhost:8080", "HTTP server address (host:port)")
-	baseURL := flag.String("b", "http://localhost:8080/", "Base URL for shortened links")
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("ошибка при парсинге переменных окружения: %v", err)
+	}
+
+	addressFlag := flag.String("a", "", "HTTP server address (host:port)")
+	baseURLFlag := flag.String("b", "", "Base URL for shortened links")
 
 	flag.Parse()
 
-	serverAddr := strings.TrimPrefix(*address, "http://")
+	if *addressFlag != "" && cfg.ServerAddress == "localhost:8080" {
+		cfg.ServerAddress = *addressFlag
+	}
+	if *baseURLFlag != "" && cfg.BaseURL == "http://localhost:8080/" {
+		cfg.BaseURL = *baseURLFlag
+	}
+
+	serverAddr := strings.TrimPrefix(cfg.ServerAddress, "http://")
 	serverAddr = strings.TrimPrefix(serverAddr, "https://")
 	serverAddr = strings.TrimSuffix(serverAddr, "/")
 
-	if serverAddr == "" || *baseURL == "" {
+	baseURL := strings.TrimSuffix(cfg.BaseURL, "/")
+
+	if serverAddr == "" || baseURL == "" {
 		return nil, fmt.Errorf("адрес и урл не предоставлены")
 	}
 
 	return &Config{
 		ServerAddress: serverAddr,
-		BaseURL:       *baseURL,
+		BaseURL:       baseURL,
 	}, nil
 }
